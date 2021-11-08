6dfa5b241c9ac732c48e56c58298765664f7425c
Nadilson <nadilson@protonmail.com>
2021-11-08 09:15:14 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/nadilson/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadilson.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
