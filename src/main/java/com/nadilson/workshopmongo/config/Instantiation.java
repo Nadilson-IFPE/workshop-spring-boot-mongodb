0cd0ce919c066eaf5b54257541cde9bc0f9f7f7e
Nadilson <nadilson@protonmail.com>
2021-11-10 16:00:28 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nadilson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nadilson/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.nadilson.workshopmongo.domain.Post;
 import com.nadilson.workshopmongo.domain.User;
 import com.nadilson.workshopmongo.dto.AuthorDTO;
+import com.nadilson.workshopmongo.dto.CommentDTO;
 import com.nadilson.workshopmongo.repository.PostRepository;
 import com.nadilson.workshopmongo.repository.UserRepository;
 
@@ -38,9 +39,16 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, nadilson, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu, viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(nadilson));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(nadilson));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
